596aec767bfa95aa230355a3bb0cb694110d2a75
Mateusz Łysień <mateusz.lysien@gmail.com>
2015-08-03 19:51:26 +0200

    Autofac has been configured.

--- FenrirProjectManager/Global.asax.cs
+++ FenrirProjectManager/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
@@ -10,13 +11,40 @@ using Autofac;
 using Autofac.Integration.Mvc;
 using DataAccessImplementation;
 using DataAccessInterfaces;
+using FenrirProjectManager.Controllers;
 
 namespace FenrirProjectManager
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private IContainer _container;
+        private ContainerBuilder _containerBuilder;
+
         protected void Application_Start()
         {
+            // build container
+            _containerBuilder = new ContainerBuilder();
+            
+            // register controllers
+            _containerBuilder.RegisterType<HomeController>().InstancePerRequest();
+            _containerBuilder.RegisterType<AccountController>().InstancePerRequest();
+            _containerBuilder.RegisterType<ManageController>().InstancePerRequest();
+            _containerBuilder.RegisterType<UsersController>().InstancePerRequest();
+            _containerBuilder.RegisterType<ProjectsController>().InstancePerRequest();
+            _containerBuilder.RegisterType<IssuesController>().InstancePerRequest();
+
+            //register services
+            _containerBuilder.RegisterType<IssueRepo>().AsSelf().As<IIssueRepo>();
+            _containerBuilder.RegisterType<ProjectRepo>().AsSelf().As<IProjectRepo>();
+            _containerBuilder.RegisterType<UserRepo>().AsSelf().As<IUserRepo>();
+
+            // building controllers
+            _container = _containerBuilder.Build();
+
+            // dependency resolver
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(_container));
+
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
